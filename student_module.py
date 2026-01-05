cae4f96a622436f4f3c36b2d20626bad6649035c
prasad-1807 <sigatapu.siva2023@vitstudent.ac.in>
2026-01-05 21:34:50 +0530

    initial

--- student_module.py
+++ student_module.py
@@ -1,7 +1,13 @@
-def register(name,rollno):
-    print("student registered")
-    print(f"roll no {rollno}")
-    print(f"name:{name}")
-name = input()
-rollno = int(input())
-register(name,rollno)
+def register(name, rollno) :
+    print("Student registered")
+    print(f"name: {name}")
+    print(f"roll no: {rollno}")
+def login(username, password):
+    if username == "admin" and password == "1234":
+        print("Login successfully")
+    else:
+        print("Invalid credentials")
+name=input()
+rollno=int(input())
+register(name, rollno)
+login(name, rollno)
